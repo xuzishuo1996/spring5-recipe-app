80bb70ca470f159b777fbfd362a83d544195314c
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-08-03 17:38:55 +0800

    add unidirectional one-to-one relation: Ingredient & UnitOfMeasure

--- src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -13,7 +13,9 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
+    //unidirectional from Ingredient to UnitOfMeasure
+    @OneToOne(fetch = FetchType.EAGER)
+    private UnitOfMeasure uom;
 
     @ManyToOne
     private Recipe recipe;
@@ -40,5 +42,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
