c9a00a48e160d8fbf7d2cc8a055da71534ecb524
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-08-04 17:54:17 +0800

    add 2 Recipes on startup

--- src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -20,6 +20,16 @@ public class Ingredient {
     @ManyToOne
     private Recipe recipe;
 
+    public Ingredient() {
+    }
+
+    public Ingredient(String description, BigDecimal amount, UnitOfMeasure uom, Recipe recipe) {
+        this.description = description;
+        this.amount = amount;
+        this.uom = uom;
+        this.recipe = recipe;
+    }
+
     public String getDescription() {
         return description;
     }
