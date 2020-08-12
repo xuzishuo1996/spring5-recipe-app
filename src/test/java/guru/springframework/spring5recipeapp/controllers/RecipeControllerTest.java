38f2ee4c06396aa68929a4785711e8926388b33d
xuzishuo1996 <xuzishuo1996@gmail.com>
2020-08-12 15:47:46 +0800

    add data validation

--- src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/RecipeControllerTest.java
@@ -91,9 +91,24 @@ class RecipeControllerTest {
                 .contentType(MediaType.APPLICATION_FORM_URLENCODED)
                 .param("id", "")
                 .param("description", "some string")
+                .param("directions", "some directions")
         )
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/recipe/2/show"));
+    }
+
+    @Test
+    public void testPostNewRecipeFormValidationFail() throws Exception {
+        RecipeCommand command = new RecipeCommand();
+        command.setId(2L);
+
+        when(recipeService.saveRecipeCommand(any())).thenReturn(command);
+
+        mockMvc.perform(post("/recipe")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED)
+                .param("id", "")    //lack @NotBlank field description and directions
+        )
+                .andExpect(status().isOk());    //validation fails, so it does not redirect
     }
 
     @Test
